02becec3d9399a912346506b2fcbac1e252c901d
godjoy <tlschgml556@gmail.com>
2020-08-21 10:27:46 +0900

    #15 kakao api 불필요한 부분 정리

--- app/src/main/java/com/mashup/nawainvitation/presentation/searchlocation/api/GetAddressResponse.kt
+++ app/src/main/java/com/mashup/nawainvitation/presentation/searchlocation/api/GetAddressResponse.kt
@@ -5,22 +5,14 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 data class GetAddressResponse(
-    val meta: Meta,
     val documents: MutableList<Documents>
-)
-
-data class Meta(
-    @SerializedName("total_count") val totalCount: Int,
-    @SerializedName("pageable_count") val pageableCount: Int,
-    @SerializedName("is_end") val isEnd: Boolean,
-    @SerializedName("same_name") val sameName: RegionInfo
 )
 
 @Parcelize
 data class Documents(
     @SerializedName("address_name") val addressName: String? = "",
     @SerializedName("road_address_name") val roadAddressName: String? = "",
-    @SerializedName("place_name") val placeName: String,
+    @SerializedName("place_name") val placeName: String?,
     val x: Double? = null,
     val y: Double? = null
 ) : Parcelable
