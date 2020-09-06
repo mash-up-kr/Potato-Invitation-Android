f532806e78d5b67dd3c59640d7767bbb1bdeb58c
sun_ny0529 <taemly93@gmail.com>
2020-09-07 01:57:32 +0900

    #14 저장된 값 있을 시 default 값 세팅, 테마 색상 변경

--- app/src/main/java/com/mashup/nawainvitation/presentation/selectdatatime/SelectingDateTimeFragment.kt
+++ app/src/main/java/com/mashup/nawainvitation/presentation/selectdatatime/SelectingDateTimeFragment.kt
@@ -2,6 +2,7 @@ package com.mashup.nawainvitation.presentation.selectdatatime
 
 import android.app.DatePickerDialog
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +20,7 @@ import com.mashup.nawainvitation.data.repository.InvitationRepositoryImpl
 import com.mashup.nawainvitation.presentation.main.MainViewModel
 import com.mashup.nawainvitation.utils.TimeUtils
 import kotlinx.android.synthetic.main.fragment_selecting_datetime.*
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -56,9 +58,13 @@ class SelectingDateTimeFragment : Fragment() {
     var userMin = "" + minNow.format(mDate)
     var userAmPm = "" + ampmNow.format(mDate)
 
-    var userDay = ""
-    var userMonth = ""
-    var userYear = ""
+    val today = Calendar.getInstance()
+    val minDate = Calendar.getInstance()
+
+    var userDay = "" + today.get(Calendar.DAY_OF_MONTH)
+    var userMonth = "" + today.get(Calendar.MONTH)
+    var userYear = "" + today.get(Calendar.YEAR)
+
 
     private var callbackMethod: DatePickerDialog.OnDateSetListener? = null
 
@@ -85,21 +91,57 @@ class SelectingDateTimeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mainViewModel.invitations.value?.let {
+            val mTime = it.invitationTime
+            if (mTime.isNullOrEmpty().not()){
+                val tvInputDate = getView()?.findViewById<TextView>(R.id.tvInputDate) as TextView
+
+                val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                val yearF = SimpleDateFormat("yyyy")
+                val monthF = SimpleDateFormat("MM")
+                val dayF = SimpleDateFormat("dd")
+                val hourF = SimpleDateFormat("HH")
+                val minuteF = SimpleDateFormat("mm")
+
+                try {
+                    val str_source = mTime //입력포멧 문자열
+                    val date_parsed = inputFormat.parse(str_source) // 문자열을 파싱해 Date형으로 저장한다
+                    val saveHourInt = hourF.format(date_parsed).toInt()
+                    val saveHour12 = if (saveHourInt < 12) saveHourInt else saveHourInt - 12
+                    userAmPm = if (saveHourInt < 12) "오전" else "오후"
+                    userHour = if (saveHour12 < 10) "0" + saveHour12 else "" + saveHour12
+                    userMin = minuteF.format(date_parsed)
+
+                    userDay = dayF.format(date_parsed)
+                    userMonth = monthF.format(date_parsed)
+                    userYear = yearF.format(date_parsed)
+
+                    val dateMsg = userYear + "년 " + userMonth + "월 " + userDay + "일"
+                    tvInputDate.text = dateMsg
+                    tvInputDate.setTextColor(Color.parseColor("#000000"))
+
+                    btnDateTimeFinish.setEnabled(true)
+
+                } catch (e: ParseException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        //date
         cvInputDate.setOnClickListener {
             this.InitializeListener()
 
-            val today = Calendar.getInstance()
-            val minDate = Calendar.getInstance()
-
+            //선택된 초기 날짜 설정
             val dialog =
                 activity?.let { it1 ->
                     DatePickerDialog(
                         it1,
                         R.style.MyDatePickerDialogStyle,
                         callbackMethod,
-                        today.get(Calendar.YEAR),
-                        today.get(Calendar.MONTH),
-                        today.get(Calendar.DAY_OF_MONTH)
+                        userYear.toInt(),
+                        userMonth.toInt()-1,
+                        userDay.toInt()
                     )
                 }
 
@@ -114,12 +156,14 @@ class SelectingDateTimeFragment : Fragment() {
             dialog?.show()
         }
 
+        //time
         OnClickTime()
 
+        //finish
         btnDateTimeFinish.setOnClickListener {
             //date
             //$userDay $userMonth $userYear
-            //ex) 202 08 04
+            //ex) 2020 08 04
 
             //time
             //$userHour $userMin $userAmPm
@@ -166,7 +210,6 @@ class SelectingDateTimeFragment : Fragment() {
                     }
                 })
         }
-
     }
 
     fun InitializeListener() {
@@ -192,6 +235,16 @@ class SelectingDateTimeFragment : Fragment() {
     private fun OnClickTime() {
         val timePicker = view?.findViewById<View>(R.id.timePicker) as TimePicker
 
+        //선택된 초기 시간 설정
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (userAmPm == "오후") { timePicker.setHour(userHour.toInt()+12)}
+            else
+                timePicker.setHour(userHour.toInt())
+
+            timePicker.setMinute(userMin.toInt())
+        }
+
+        //listner
         timePicker.setOnTimeChangedListener { _, hour, minute ->
             var hour = hour
             var am_pm = ""
